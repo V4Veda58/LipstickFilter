1c827dfc019d7fc0be8272fb511e41a728768379
V4Veda58 <85873657+V4Veda58@users.noreply.github.com>
2022-01-31 15:38:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,28 @@ function preload(){
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
+    video =  createCapture(VIDEO);
+    video.size(300, 300);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
 
 }
 function take_Snapshot(){
     save("LipstickFliter.png");
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
 }
